589e4208bad1bd3f13c39b5b43f6bea271d39348
Steiley <xwarina.sivik@gmail.com>
2017-11-02 18:25:58 +0900

    UsersControllerのactionとviewを追加した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render :index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
   end
 end
