c6c945c591e4183c06cc8cd8faa4adf602cd3a16
steiley <xwarina.sivik@gmail.com>
2020-01-04 17:13:10 +0900

    Automatically updated by rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   # updateの時はパスワードは必須でなくて良い
   before_validation do |record|
     next if record.new_record?
+
     record.password = nil if !record.password.nil? && record.password.empty?
     record.password_confirmation = nil if !record.password_confirmation.nil? && record.password_confirmation.empty?
   end
