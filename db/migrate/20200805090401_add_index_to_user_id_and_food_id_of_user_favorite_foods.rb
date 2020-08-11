5a6853ef2e20cb8c0e2d04c2d4128296fc6a0359
steiley <xwarina.sivik@gmail.com>
2020-08-11 17:49:35 +0900

    Fix for Rubocop Rails/BulkChangeTable cop

--- db/migrate/20200805090401_add_index_to_user_id_and_food_id_of_user_favorite_foods.rb
+++ db/migrate/20200805090401_add_index_to_user_id_and_food_id_of_user_favorite_foods.rb
@@ -1,6 +1,8 @@
 class AddIndexToUserIdAndFoodIdOfUserFavoriteFoods < ActiveRecord::Migration[6.0]
   def change
-    add_index :user_favorite_foods, :user_id
-    add_index :user_favorite_foods, :food_id
+    change_table :user_favorite_foods, bulk: true do |t|
+      t.index :user_id
+      t.index :food_id
+    end
   end
 end
